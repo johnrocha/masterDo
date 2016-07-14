fc4663b7a2f6dd0798cba1a90e947f6fd0c5fd5f
Rafael Uchôa <rafael@overmediacast.com>
2016-07-14 16:56:30 -0300

    Cria um array de Lista com "_id" e "nome" e mostra cada um em um item

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,11 @@
 angular.module('app.controllers', [])
   
 .controller('MinhasListasCtrl', function($scope) {
+  $scope.minhasListas = [
+    { _id: '1', name: 'Lista de compras 1' },
+    { _id: '2', name: 'Lista de compras 2' },
+  ];
+
 
 })
    
